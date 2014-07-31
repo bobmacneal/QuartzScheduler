5c286d003589494995deb49c217c3f25bb6bf28c
bobmacneal <bob.macneal@workingcompany.com>
2014-07-31 13:58:19 -0500

    Formatting

--- QuartzSpike/App_Start/UnityConfig.cs
+++ QuartzSpike/App_Start/UnityConfig.cs
@@ -11,6 +11,7 @@ namespace QuartzSpike
             var container = new UnityContainer();
             Jobs.Config.UnityConfig.RegisterComponents(container);
             Services.Config.UnityConfig.RegisterComponents(container);
+
             QuartzScheduler.InitializeQuartzJobs(container);
             //Wire up Unity in the global configuration of Web Api
             GlobalConfiguration.Configuration.DependencyResolver = new UnityDependencyResolver(container);
